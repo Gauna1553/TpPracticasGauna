715137bcf06e998230ed5d549ed6e4d876efaeec
Gauna1553 <gabrielgauna04@gmail.com>
2023-07-05 09:35:59 -0300

    cree los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemComponent } from './components/item/item.component';
+import { ItemsComponent } from './components/items/items.component';
+import { TotalComponent } from './components/total/total.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemComponent,
+    HeaderComponent,
+    ItemComponent,
+    ItemsComponent,
+    TotalComponent
   ],
   imports: [
     BrowserModule,
