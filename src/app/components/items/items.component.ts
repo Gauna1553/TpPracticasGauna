ce9e1ac1ca1e602a81f8102b18d527c8acf274a6
Gauna1553 <gabrielgauna04@gmail.com>
2023-07-07 09:46:27 -0300

    creamos un item en ts

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-items',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent {
+
+  items: Item[] = [];
 
 }
